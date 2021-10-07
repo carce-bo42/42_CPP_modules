6da94f7e7245a68bf0699cae4d8b7eac8b0fe195
Christian Yerildt Arce Borro <yerildtt@gmail.com>
2021-10-07 15:22:34 +0200

    ex02 completed but not testes, lots errors to cover i assume

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -69,6 +69,13 @@ int	Fixed::toInt( void ) const {
 // Operator overloads //
 ////////////////////////
 
+Fixed &		Fixed::operator = ( Fixed const &rhs ) {
+
+	this->_fpvalue = rhs.getRawBits();
+	return *this ;
+
+}
+
 bool	Fixed::operator > ( Fixed const	&rhs) {
 
 	return ( this->getRawBits() > rhs.getRawBits() ) ;
@@ -105,14 +112,121 @@ bool	Fixed::operator != ( Fixed const &rhs ) {
 
 }
 
-Fixed &		Fixed::operator=( Fixed const &rhs ) {
+Fixed	Fixed::operator + ( Fixed const &rhs ) {
 
-	this->_fpvalue = rhs.getRawBits();
-	return *this ;
+	return ( Fixed(this->toInt() + rhs.toInt()) ) ;
 
 }
 
-std::ostream&	operator<<( std::ostream & o, Fixed const &rhs ) {
+Fixed	Fixed::operator - ( Fixed const &rhs ) {
+
+	return ( Fixed(this->toInt() - rhs.toInt()) ) ;
+
+}
+
+// We lose precision in exchange for range. Multiplication will have
+// a max precision of 2^-4 = 0.0625, and the maximum whole part, which
+// was initially 2^(31 - 8) -1 = 8 388 607, is now 2^(31 - 8 - 4) -1 = 524 287.
+
+Fixed	Fixed::operator * ( Fixed const &rhs ) {
+
+	Fixed	output;
+	int		aux_1;
+	int		aux_2;
+
+	aux_1 = this->getRawBits() >> (int)(Fixed::_fbits / 2) ;
+	aux_2 = rhs.getRawBits() >> (int)(Fixed::_fbits / 2) ;
+
+	output.setRawBits( aux_1 * aux_2 );
+
+	return ( output ) ;
+}	
+
+Fixed	Fixed::operator / ( Fixed const &rhs ) {
+
+	Fixed	output;
+	int		aux_1;
+	int		aux_2;
+
+	aux_1 = this->getRawBits() << (int)(Fixed::_fbits / 2) ;
+	aux_2 = rhs.getRawBits() << (int)(Fixed::_fbits / 2) ;
+
+	output.setRawBits( aux_1 / aux_2 );
+
+	return ( output ) ;
+
+}
+
+// 1 = 00000000 00000000 00000000 00000001, which interpreted
+// as fixed point is the least representable positive amount that
+// this format can handle.
+Fixed&	Fixed::operator ++ ( void ) {
+
+	this->setRawBits( this->getRawBits + 1 );
+	return ( *this ) ;
+
+}
+
+Fixed	Fixed::operator ++ ( int ) {
+
+	Fixed	output = Fixed(*this);
+	++(*this);
+
+	return ( output ) ;
+
+}
+
+Fixed&	Fixed::operator -- ( void ) {
+
+	this->setRawBits( this->getRawBits - 1 );
+	return ( *this ) ;
+
+}
+
+Fixed	Fixed::operator -- ( int ) {
+
+	Fixed	output = Fixed(*this);
+	--(*this);
+
+	return ( output ) ;
+
+}
+
+//////////////////////
+// Class Functions //
+/////////////////////	
+
+Fixed&	Fixed::min( Fixed const &x, Fixed const &y ) {
+
+	return ( (x < y) ? x : y ) ;
+
+}
+
+Fixed&	Fixed::max( Fixed const &x, Fixed const &y ) {
+
+	return ( (x > y) ? x : y ) ;
+
+}
+
+///////////////////////////////
+// Outter operator overloads//
+//////////////////////////////
+
+const Fixed&	min( Fixed const &x, Fixed const &y ) {
+
+	return ( (x < y) ? x : y ) ;
+
+}
+
+
+const Fixed&	max( Fixed const &x, Fixed const &y ) {
+
+	return ( (x > y) ? x : y ) ;
+
+}
+
+
+std::ostream&	operator << ( std::ostream & o, Fixed const &rhs ) {
 
 	o << rhs.toFloat();
 	return o;
