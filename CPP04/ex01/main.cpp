afd1a45e407f1e0ecb7c68d04815df44202beafa
Christian Yerildt Arce Borro <yerildtt@gmail.com>
2021-10-11 19:39:00 +0200

    ex01 works

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -4,19 +4,11 @@
 
 int main()
 {
-	std::cout << "Example:" << std::endl;
-	std::cout << std::endl;
-	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete meta;
+	delete j;//should not create a leak
+	system("leaks Animals_test2");
 	delete i;
-	delete j;
-	//system("leaks Animals_test1");
+	system("leaks Animals_test2");
 	return 0;
 }
