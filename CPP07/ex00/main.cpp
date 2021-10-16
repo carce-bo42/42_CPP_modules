6cf1a0232697c5bfe63053813d3e01ff7aa97e65
Christian Yerildt Arce Borro <yerildtt@gmail.com>
2021-10-16 15:51:29 +0200

    added intra's main too

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -9,10 +9,9 @@ int main() {
 	// INT demonstrartion.
 	std::cout << "Min of (" << i << " , " << j << "): " << min<int>(i,j) << std::endl;
 	std::cout << "Max of (" << i << " , " << j << "): " << max<int>(i,j) << std::endl;
-	std::cout << "i = " << i << " and " << "j = " << j << std::endl;
+	std::cout << "Before swapping: " << "i = " << i << " and " << "j = " << j << std::endl;
 	swap<int>(i,j);
-	std::cout << "After swapping:" << std::endl;
-	std::cout << "i = " << i << " and " << "j = " << j << std::endl;
+	std::cout << "After swapping: " << "i = " << i << " and " << "j = " << j << std::endl;
 
 	std::cout << std::endl;
 	}
@@ -25,24 +24,43 @@ int main() {
 	std::cout << std::setiosflags( std::ios::fixed );
 	std::cout << "Min of (" << i << " , " << j << "): " << min<float>(i,j) << std::endl;
 	std::cout << "Max of (" << i << " , " << j << "): " << max<float>(i,j) << std::endl;
-	std::cout << "i = " << i << " and " << "j = " << j << std::endl;
+	std::cout << "Before swapping: " << "i = " << i << " and " << "j = " << j << std::endl;
 	swap<float>(i,j);
-	std::cout << "After swapping:" << std::endl;
-	std::cout << "i = " << i << " and " << "j = " << j << std::endl;
+	std::cout << "After swapping: " << "i = " << i << " and " << "j = " << j << std::endl;
 
 	std::cout << std::endl;
 	}
+	{
 	char	i = 'a';
 	char	j = 'b';
 
 	// char demonstrartion.
 	std::cout << "Min of (" << i << " , " << j << "): " << min<char>(i,j) << std::endl;
 	std::cout << "Max of (" << i << " , " << j << "): " << max<char>(i,j) << std::endl;
-	std::cout << "i = " << i << " and " << "j = " << j << std::endl;
+	std::cout << "Before swapping: " << "i = " << i << " and " << "j = " << j << std::endl;
 	swap<char>(i,j);
-	std::cout << "After swapping:" << std::endl;
-	std::cout << "i = " << i << " and " << "j = " << j << std::endl;
+	std::cout << "After swapping: " << "i = " << i << " and " << "j = " << j << std::endl;
 
 	std::cout << std::endl;
+	std::cout << std::endl;
+	}
+	// Subject main:
+	{
+		int a = 2;
+		int b = 3;
+
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
 	return 0;
 }
