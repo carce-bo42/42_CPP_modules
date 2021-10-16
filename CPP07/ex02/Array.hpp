45a94e5814b7a64b7f6c89b300347204600117d5
Christian Yerildt Arce Borro <yerildtt@gmail.com>
2021-10-16 17:55:39 +0200

    Array template done

---
+++ CPP07/ex02/Array.hpp
@@ -0,0 +1,62 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+
+template < typename T >
+class Array {
+
+	public:
+
+		Array( void ) : A( new T[0] ), _size( 0 ) {};
+
+		Array( unsigned int n ) : A( new T[n] ), _size( n ) {};
+
+		Array( Array<T> const &other ) : A( new T[other.size()] ), _size( other.size() ) {
+
+			for (unsigned int i=0; i<other.size(); i++) {
+				this->A[i] = other[i];
+			}
+		}
+
+		~Array( void ) { delete [] this->A; }
+
+		Array<T>& operator = (Array<T> const &other) {
+
+			if (this != &other) {
+
+				delete [] this->A;
+				A = new T[other.size()];
+				this->_size = other.size();
+
+				for (unsigned int i=0; i<other.size(); i++) {
+					this->A[i] = other[i];
+				}
+			}
+			return *this;
+		}
+
+		T&	operator [] (unsigned int i) const {
+
+			if (i >= this->_size) { throw IndexOutOfBounds(); }
+			else { return this->A[i]; }
+		}
+
+		unsigned int	size( void ) const { return this->_size; }
+
+		// Index out of bounds exception: 
+
+		class	IndexOutOfBounds : public std::exception {
+			public:
+				virtual const char* what() const throw() {
+					return( "Index out of bounds" );
+				}
+		};
+
+	private:
+
+		T*				A;
+		unsigned int	_size;
+};
+			
+#endif
---
+++ CPP07/ex02/main.cpp
@@ -0,0 +1,119 @@
+#include "Array.hpp"
+#include "stdlib.h"
+#define MAX_VAL 750
+
+void	CheckLeaks( void ) {
+
+	system( "leaks Templates3" );
+}
+
+int main() {
+
+	atexit( CheckLeaks );
+	// MY MAIN (ONLY FOR INT BUT I THINK IT WAS NEVER THE POINT).
+	{
+	Array<int>	a( 3 );
+
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+
+	std::cout << "size of a is: " << a.size() << std::endl;
+	std::cout << "a: ";
+	for (int i=0; i<3; i++) {
+		std::cout << "a[" << i << "] = " << a[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Assigning a to a new empty array:" << std::endl;
+	std::cout << std::endl;
+
+	Array<int> b; // No parameters.
+
+	std::cout << "size of b (b = a) is: " << a.size() << std::endl;
+	std::cout << "b: ";
+
+	b = a;
+	for (int i=0; i<3; i++) {
+		std::cout << "b[" << i << "] = " << b[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Contrucion by copy: " << std::endl;
+	std::cout << std::endl;
+
+	Array<int>	c( b );
+
+	std::cout << "size of c( a ) is: " << a.size() << std::endl;
+	std::cout << "c: ";
+
+	for (int i=0; i<3; i++) {
+		std::cout << "c[" << i << "] = " << c[i] << " ";
+	}
+	
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	try {
+		std::cout << "Trying to access an index out of bounds: " << std::endl;
+		std::cout << std::endl;
+
+		std::cout << a[3] << std::endl;
+	} catch (Array<int>::IndexOutOfBounds &e ) {
+		std::cout << e.what() << std::endl;
+	}
+	}
+	/////////////////////////////////////////777777777777777777777777777/////
+	////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////
+	// THE INTRA MAIN:
+	Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;
+	return 0;
+}
